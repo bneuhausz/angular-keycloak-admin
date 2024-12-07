895bb9d9159ec818e3c538e2ee850ffe3d28a2bb
nb <bneuhausz@gmail.com>
2024-12-07 22:14:02 +0100

    added user table

---
+++ src/app/user-management/ui/user-table.component.ts
@@ -0,0 +1,85 @@
+import { Component, input } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { User } from "../interfaces/user";
+
+@Component({
+  selector: "app-user-table",
+  imports: [MatTableModule, MatCardModule, MatProgressSpinnerModule, MatCheckboxModule],
+  template: `
+    <main>
+      <mat-card>
+        @if (loading()) {
+          <section class="spinner-container">
+            <mat-progress-spinner mode="indeterminate" diameter="100"></mat-progress-spinner>
+          </section>
+        }
+        @else {
+          <table mat-table [dataSource]="users()">
+            <ng-container matColumnDef="id">
+              <th mat-header-cell *matHeaderCellDef>ID</th>
+              <td mat-cell *matCellDef="let user">
+                {{ user.id }}
+              </td>
+            </ng-container>
+            <ng-container matColumnDef="username">
+              <th mat-header-cell *matHeaderCellDef>Username</th>
+              <td mat-cell *matCellDef="let user">
+                {{ user.username }}
+              </td>
+            </ng-container>
+            <ng-container matColumnDef="enabled">
+              <th mat-header-cell *matHeaderCellDef>Enabled</th>
+              <td mat-cell *matCellDef="let user">
+                <mat-checkbox [checked]="user.enabled" disabled>
+
+                </mat-checkbox>
+              </td>
+            </ng-container>
+            <tr mat-header-row *matHeaderRowDef="displayedColumns" id="user-table-header"></tr>
+            <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+          </table>
+        }
+      </mat-card>
+    </main>
+  `,
+  styles: [
+    `
+      main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      mat-card {
+        width: 800px;
+        margin: 20px 10px;
+        padding: 20px 0;
+      }
+
+      .spinner-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+      }
+
+      table {
+        width: 100%;
+      }
+
+      #user-table-header {
+        background-color: var(--mat-sys-primary);
+        color: var(--mat-sys-on-primary);
+      }
+    `,
+  ],
+})
+export class UserTableComponent {
+  users = input.required<User[]>();
+  loading = input.required<boolean>();
+
+  displayedColumns = ['id', 'username', 'enabled'];
+}
--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserManagementService } from './data-access/user-management.service';
+import { UserTableComponent } from './ui/user-table.component';
 
 @Component({
-  selector: 'app-user-management',
+  imports: [UserTableComponent],
+  providers: [UserManagementService],
   template: `
     <p>User Management</p>
+    <app-user-table [users]="userManagementService.users()" [loading]="userManagementService.loading()" />
   `,
 })
 export default class UserManagementComponent {
+  userManagementService = inject(UserManagementService);
 }
