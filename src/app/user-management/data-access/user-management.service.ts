4de8d670a5b90f23d921561fa63e1675399a5aea
nb <bneuhausz@gmail.com>
2024-12-11 15:00:14 +0100

    started implementing pagination

--- src/app/user-management/data-access/user-management.service.ts
+++ src/app/user-management/data-access/user-management.service.ts
@@ -2,15 +2,16 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { computed, inject, Injectable, signal } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { KeycloakService } from "keycloak-angular";
-import { catchError, EMPTY, from, Subject, switchMap, tap, withLatestFrom } from "rxjs";
+import { catchError, combineLatest, EMPTY, from, startWith, Subject, switchMap, tap, withLatestFrom } from "rxjs";
 import { CreateUser, User } from "../interfaces/user";
 import { environment } from "../../../environments/environment.development";
+import { Pagination } from "../../shared/interfaces/pagination";
 
 interface UserManagementState {
   users: User[];
   loading: boolean;
   error: string | null;
-  
+  pagination: Pagination;
 }
 
 @Injectable()
@@ -22,12 +23,26 @@ export class UserManagementService {
     users: [],
     loading: true,
     error: null,
+    pagination: {
+      total: 0,
+      page: 1,
+      pageSize: 5,
+    }
   });
 
   users = computed(() => this.#state().users);
   loading = computed(() => this.#state().loading);
   //TODO: snackbar for errors
   error = computed(() => this.#state().error);
+  pagination = computed(() => this.#state().pagination);
+
+  private readonly userCount$ = from(this.keycloakService.getToken())
+    .pipe(
+      takeUntilDestroyed(),
+      switchMap((token) =>
+        this.userCount(token)
+      ),
+    );
 
   loadUsers$ = from(this.keycloakService.getToken())
     .pipe(
@@ -99,6 +114,16 @@ export class UserManagementService {
       );
 
     constructor() {
+      this.userCount$.subscribe((total) => {
+        this.#state.update((state) => ({
+          ...state,
+          pagination: {
+            ...state.pagination,
+            total,
+          },
+        }));
+      });
+
       this.loadUsers$.subscribe();
 
       this.userCreated$.subscribe();
@@ -107,7 +132,9 @@ export class UserManagementService {
     }
 
   private getUsers(token: string) {
-    return this.http.get<User[]>(`${environment.keycloakConfig.userManagementBaseUrl}`, {
+    const first = (this.pagination().page - 1) * this.pagination().pageSize;
+    const max = this.pagination().page * this.pagination().pageSize;
+    return this.http.get<User[]>(`${environment.keycloakConfig.userManagementBaseUrl}?first=${first}&max=${max}`, {
       headers: new HttpHeaders({ Authorization: `Bearer ${token}` })
     });
   }
@@ -116,14 +143,21 @@ export class UserManagementService {
     return this.http.post(
       `${environment.keycloakConfig.userManagementBaseUrl}`,
       user,
-      { headers: new HttpHeaders({ Authorization: `Bearer ${token}` })}
+      { headers: new HttpHeaders({ Authorization: `Bearer ${token}` }) }
     );
   }
 
   private deleteUser(token: string, userId: string) {
     return this.http.delete(
       `${environment.keycloakConfig.userManagementBaseUrl}/${userId}`,
-      { headers: new HttpHeaders({ Authorization: `Bearer ${token}` })}
+      { headers: new HttpHeaders({ Authorization: `Bearer ${token}` }) }
+    );
+  }
+
+  private userCount(token: string) {
+    return this.http.get<number>(
+      `${environment.keycloakConfig.userManagementBaseUrl}/count`,
+      { headers: new HttpHeaders({ Authorization: `Bearer ${token}` }) }
     );
   }
 }
