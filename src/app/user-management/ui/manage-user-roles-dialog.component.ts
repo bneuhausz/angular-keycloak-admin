3e24bda6150497a87af997ee3fc54a381285e1cc
nb <bneuhausz@gmail.com>
2024-12-13 10:49:25 +0100

    minor user-role management design imporvement

--- src/app/user-management/ui/manage-user-roles-dialog.component.ts
+++ src/app/user-management/ui/manage-user-roles-dialog.component.ts
@@ -4,18 +4,19 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { EditUserRole, Role } from "../../shared/interfaces/role";
 
-//TODO: improve the design
 @Component({
   host: { 'data-dialog': 'manage-user-roles' },
   imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, MatCheckboxModule],
   template: `
     <h2 mat-dialog-title align="center">Manage User Roles</h2>
     <mat-dialog-content align="center">
-      @for (role of data.realmRoles; track role.id) {
-        <mat-checkbox [checked]="isUserInRole(role.id)" (change)="this.roleToggled.emit({ userId: this.data.user.id, roleId: role.id, roleName: role.name, checked: $event.checked })">
-          {{ role.name }}
-        </mat-checkbox>
-      }
+      <section>
+        @for (role of data.realmRoles; track role.id) {
+          <mat-checkbox [checked]="isUserInRole(role.id)" (change)="this.roleToggled.emit({ userId: this.data.user.id, roleId: role.id, roleName: role.name, checked: $event.checked })">
+            {{ role.name }}
+          </mat-checkbox>
+        }
+      </section>
     </mat-dialog-content>
     <mat-dialog-actions align="center">
       <button mat-raised-button mat-dialog-close>Close</button>
@@ -24,11 +25,26 @@ import { EditUserRole, Role } from "../../shared/interfaces/role";
   styles: [
     `
       mat-dialog-content {
-        height: 300px;
+        height: 100%;
         width: 100%;
         display: flex;
         flex-direction: column;
+        align-items: center;
+        padding: 20px;
+      }
+
+      section {
+        width: fit-content;
+        min-width: 150px;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
         gap: 10px;
+      }
+
+      mat-checkbox {
+        margin: 5px 0;
       }
     `
   ]
